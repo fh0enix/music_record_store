c058b05429fd892a552d56afa91329733a036713
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2024-06-13 15:34:59 +0300

    "Updated db/seeds.rb to create admin, manager, and regular users, and a music record."

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.create!(first_name: "Admin", last_name: "User", email: "admin@example.com", role: "admin", password: "password")
+User.create!(first_name: "Manager", last_name: "User", email: "manager@example.com", role: "manager", password: "password")
+User.create!(first_name: "Regular", last_name: "User", email: "user@example.com", role: "user", password: "password")
+
+MusicRecord.create!(title: "Record1", price: 10.0, description: "First Record", damage_assessment: "New")
