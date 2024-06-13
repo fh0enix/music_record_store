cd1b1081e225f89607647ec672e0993833f05a51
Oleksandr Lutsyuk <oleksandr.lutsyk@gmail.com>
2024-06-13 15:29:11 +0300

    "Added enum for user roles and validations for first_name and last_name in User model"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum role: { user: 'user', manager: 'manager', admin: 'admin' }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, presence: true
 end
